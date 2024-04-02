0ff162b6d9d44bb228682a4d84be2b144730bb9e
EwigLife <96788793+syedzainqadri@users.noreply.github.com>
2024-04-03 00:19:22 +0500

    pushed after migration

---
+++ script.js
@@ -0,0 +1,26 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main(
+    username,
+    password
+) {
+  const user = await prisma.user.create({
+    data: {
+      name: username,
+      email: password,
+    },
+  })
+  console.log(user)
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
